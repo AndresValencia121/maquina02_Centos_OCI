443a33cd9b67e053a365ac6a14aa64d0bc5e4ac5
ANDRES VALENCIA TRUJILLO <andres.valencia121@gmail.com>
2021-10-14 12:36:56 -0500

    Proyecto en clase del 13-10-2021

---
+++ src/main/java/misiontic2022g25/proyectoclase13102021/Proyectoclase13102021Application.java
@@ -0,0 +1,13 @@
+package misiontic2022g25.proyectoclase13102021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectoclase13102021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectoclase13102021Application.class, args);
+	}
+
+}
---
+++ src/test/java/misiontic2022g25/proyectoclase13102021/Proyectoclase13102021ApplicationTests.java
@@ -0,0 +1,13 @@
+package misiontic2022g25.proyectoclase13102021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectoclase13102021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
